bebd16287128952e552c76c103ba0a3de909334d
Autch <autch@autch.net>
2012-05-06 01:34:31 +0900

    removed ACTION_SEND

--- src/net/autch/android/web_uncomp/RedirectActivity.java
+++ src/net/autch/android/web_uncomp/RedirectActivity.java
@@ -6,59 +6,23 @@ import android.net.Uri;
 import android.os.Bundle;
 
 public class RedirectActivity extends Activity {
-	private static final int REQUEST_CHOOSER = 0x100;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		Intent it = getIntent();
-		Intent it_to_send = new Intent(it);
-		String url = it.getDataString();
-		String action = it.getAction();
+		Uri from_uri = it.getData();
 		
-		if(action.equals(Intent.ACTION_SEND)) {
-			url = it.getExtras().getString(Intent.EXTRA_STREAM);
-			if(url == null) {
-				url = it.getExtras().getString(Intent.EXTRA_TEXT);
-				it_to_send.putExtra(Intent.EXTRA_TEXT, appendUncompressKey(url));
-			} else {
-				it_to_send.putExtra(Intent.EXTRA_STREAM, appendUncompressKey(url));
-			}
+		if(from_uri != null) {
+			startActivity(createRedirectIntent(from_uri));
 		}
-		if(action.equals(Intent.ACTION_VIEW)) {
-			url = it.getDataString();
-			it_to_send.setData(Uri.parse(appendUncompressKey(url)));
-		}
-		
-		Intent chooser = Intent.createChooser(it_to_send, "ブラウザの選択");
-		startActivityForResult(chooser, REQUEST_CHOOSER);
 	}
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch(requestCode) {
-		case REQUEST_CHOOSER:
-			if(resultCode == RESULT_OK) {
-				Intent intent = (Intent) data.getExtras().get(Intent.EXTRA_INTENT);
-				if(intent != null) {
-					startActivity(intent);
-				}
-			}
-			break;
-		default:
-			break;
-		}
-
-		finish();
-	}
-
-	private String appendUncompressKey(String url) {
-		if(url.indexOf("?") != -1) {
-			url += "&mpswc=cHI9b3Jn";
-		} else {
-			url += "?mpswc=cHI9b3Jn";
-		}
-		return url;
+	private Intent createRedirectIntent(Uri from_uri) {
+		Uri.Builder builder = from_uri.buildUpon();
+		builder.appendQueryParameter("mpswc", "cHI9b3Jn");
+		Intent it = new Intent(Intent.ACTION_VIEW, builder.build());
+		it.addCategory(Intent.CATEGORY_DEFAULT);
+		return it;		
 	}
 }
